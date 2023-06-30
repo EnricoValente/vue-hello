6155e6ec329115f149ba2ee72d09fce0c8d1e76e
EnricoValente <enricovalente42@gmail.com>
2023-06-30 16:32:36 +0200

    aggiunta immagine

--- js/script.js
+++ js/script.js
@@ -5,7 +5,13 @@ const{createApp} = Vue;
 const app = createApp({
     data() {
         return{
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+
+            image:'https://picsum.photos/seed/picsum/200/300',
+            
+            imageContainer:   'img-container',
+
+            imageStyle:'picsum-img',
         }
     }
 }).mount('#app');
